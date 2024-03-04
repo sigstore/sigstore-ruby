dd5b2302f9db1057791fe911ce8387700be2b027
Samuel Giddins <segiddins@segiddins.me>
2024-03-04 15:25:07 -0800

    Try to get CI passing

--- lib/sigstore/internal/debug.rb
+++ lib/sigstore/internal/debug.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Sigstore
   module Internal
     module Debug
--- lib/sigstore/internal/json.rb
+++ lib/sigstore/internal/json.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module Sigstore::Internal::JSON
+  # Implements https://wiki.laptop.org/go/Canonical_JSON
   def self.canonical_generate(data)
     case data
     when NilClass
@@ -14,13 +15,16 @@ module Sigstore::Internal::JSON
     when String
       "\"#{data.gsub(/(["\\])/, '\\\\\1')}\""
     when Array
-      "[" + data.map { |v| canonical_generate(v) }.join(",") + "]"
+      contents = data.map { |v| canonical_generate(v) }.join(",")
+      "[#{contents}]"
     when Hash
-      "{" +
-        data.sort_by { |k, _|
-          k.encode("utf-16").codepoints
-        }.map { |k, v| "#{canonical_generate(k)}:#{canonical_generate(v)}" }.join(",") +
-        "}"
+      contents = data.sort_by do |k, _|
+        k.encode("utf-16").codepoints
+      end
+      contents.map! do |k, v|
+        "#{canonical_generate(k)}:#{canonical_generate(v)}"
+      end
+      "{#{contents.join(",")}}"
     else
       raise ArgumentError, "Unsupported data type: #{data.class}"
     end
