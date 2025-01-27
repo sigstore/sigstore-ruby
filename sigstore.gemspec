409ac01a18d6f0adbd436d56cc91507d01493ce1
Samuel Giddins <segiddins@segiddins.me>
2025-01-27 09:04:14 -0800

    Fix xfail list for ruby 3.4 (#207)
    * Fix xfail list for ruby 3.4
    
    Signed-off-by: Samuel Giddins <segiddins@segiddins.me>
    
    * Update conformance test version
    
    To a version that is able to pass on CI
    
    Signed-off-by: Samuel Giddins <segiddins@segiddins.me>
    
    * Handle detailed_message missing from ruby 3.1
    
    Signed-off-by: Samuel Giddins <segiddins@segiddins.me>
    
    * Add dependency on logger for ruby 3.5
    
    Signed-off-by: Samuel Giddins <segiddins@segiddins.me>
    
    ---------
    
    Signed-off-by: Samuel Giddins <segiddins@segiddins.me>

--- sigstore.gemspec
+++ sigstore.gemspec
@@ -27,6 +27,7 @@ Gem::Specification.new do |spec|
   spec.executables = spec.files.grep(%r{\Aexe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
 
+  spec.add_dependency "logger"
   spec.add_dependency "net-http"
   spec.add_dependency "protobug_sigstore_protos", "~> 0.1.0"
   spec.add_dependency "uri"
--- test/sigstore/conformance_test.rb
+++ test/sigstore/conformance_test.rb
@@ -11,32 +11,14 @@ class Sigstore::ConformanceTest < Test::Unit::TestCase
           e = assert_raise SystemExit do
             Sigstore::CLI.start([
                                   "verify",
-                                  "--signature", "test/sigstore-conformance/test/assets/a.txt.invalid.sig",
-                                  "--certificate", "test/sigstore-conformance/test/assets/a.txt.invalid.crt",
                                   "--certificate-identity", "https://github.com/sigstore-conformance/extremely-dangerous-public-oidc-beacon/.github/workflows/extremely-dangerous-oidc-beacon.yml@refs/heads/main",
                                   "--certificate-oidc-issuer", "https://token.actions.githubusercontent.com",
+                                  "--bundle",
+                                  "test/sigstore-conformance/test/assets/a.txt.invalid_signature.sigstore.json",
                                   "test/sigstore-conformance/test/assets/a.txt"
                                 ])
           end
           assert_equal 1, e.status
-        end
-      end
-    end
-  end
-
-  def test_verify_success
-    VCR.use_cassette("conformance/verify_signature_success") do
-      capture_output do
-        assert_nothing_raised do
-          Sigstore::CLI.start([
-                                "verify",
-                                "--signature", "test/sigstore-conformance/test/assets/a.txt.good.sig",
-                                "--certificate", "test/sigstore-conformance/test/assets/a.txt.good.crt",
-                                "--certificate-identity", "https://github.com/sigstore-conformance/extremely-dangerous-public-oidc-beacon/.github/workflows/extremely-dangerous-oidc-beacon.yml@refs/heads/main",
-                                "--certificate-oidc-issuer", "https://token.actions.githubusercontent.com",
-                                "--trusted-root", "test/sigstore-conformance/test/assets/trusted_root.public_good.json",
-                                "test/sigstore-conformance/test/assets/a.txt"
-                              ])
         end
       end
     end
